b017c07de4ecdb4f7c0e68f70954bb0ee02b5f55
Anders Nõu <anders.nou@gmail.com>
2024-05-09 22:22:57 +0200

    change test config

--- micro-benchmarks/postgres/init-scripts/init_world_db.sql
+++ micro-benchmarks/postgres/init-scripts/init_world_db.sql
@@ -1,22 +1,6 @@
 CREATE DATABASE world;
 
--- CREATE USER dbuser WITH ENCRYPTED PASSWORD 'dbpassword';
--- GRANT ALL PRIVILEGES ON DATABASE world TO dbuser;
-
 \c world;
-
-CREATE TABLE World (
-    id SERIAL PRIMARY KEY,
-    randomNumber INT NOT NULL
-);
-
-INSERT INTO World (randomNumber)
-SELECT floor(random() * 10000 + 1)::int
-FROM generate_series(1, 10000);
-
--- TechEmpower settings
-CREATE DATABASE hello_world;
-\c hello_world;
 
 CREATE TABLE World (
     id SERIAL PRIMARY KEY,
