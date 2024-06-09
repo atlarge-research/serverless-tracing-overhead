b96fac1083f7bec2d057e603873621f9181efb69
andersnou <anders.nou@gmail.com>
2024-06-09 22:41:26 +0200

    wip

--- task-based-experiment/benchmarks/600.opentelemetry/650.compression-opentelemetry/nodejs/function.js
+++ task-based-experiment/benchmarks/600.opentelemetry/650.compression-opentelemetry/nodejs/function.js
@@ -14,7 +14,7 @@ const { Resource } = require('@opentelemetry/resources');
 
 const provider = new NodeTracerProvider({
   resource: new Resource({
-    'service.name': '650.compression-opentelemetry',
+    'service.name': '650.compression-opentelemetry-nodejs',
   }),
 });
 const exporter = new CollectorTraceExporter({
@@ -32,14 +32,12 @@ const readdir = util.promisify(fs.readdir);
 async function parseDirectory(directory) {
   return tracer.startActiveSpan('parse_directory', async (span) => {
     let size = 0;
-    console.log('Parsing directory:', directory);
     const files = await readdir(directory);
     for (const file of files) {
       const filePath = path.join(directory, file);
       const fileStat = await stat(filePath);
       if (fileStat.isFile()) {
         size += fileStat.size;
-        console.log('File:', filePath, 'Size:', fileStat.size);
       }
     }
     span.setAttribute('directory_size', size);
@@ -54,12 +52,10 @@ async function compressDirectory(sourceDir, archivePath) {
     const archive = archiver('zip', { zlib: { level: 9 } });
 
     output.on('close', () => {
-      console.log(`Archive ${archivePath} created successfully, size: ${archive.pointer()} bytes`);
       resolve(archive.pointer());
     });
 
     archive.on('error', (err) => {
-      console.error('Error creating archive:', err);
       reject(err);
     });
 
@@ -87,41 +83,32 @@ exports.handler = async function(event) {
         'download_path': download_path,
       });
 
-      console.log('Event attributes set:', { bucket, input_prefix, output_prefix, key, download_path });
 
       const s3DownloadBegin = Date.now();
-      console.log('Starting S3 download...');
       await storage_handler.downloadDirectory(bucket, path.join(input_prefix, key), download_path);
       const s3DownloadStop = Date.now();
       const downloadTime = (s3DownloadStop - s3DownloadBegin) / 1000;
       span.addEvent('S3 download completed', { 'download_time': downloadTime });
-      console.log('S3 download completed in', downloadTime, 'seconds');
 
       const size = await parseDirectory(download_path);
       span.setAttribute('download_size', size);
-      console.log('Directory size:', size);
 
       const compressBegin = Date.now();
-      console.log('Starting compression...');
       const archivePath = path.join('/tmp', `${key}.zip`);
       const archiveSize = await compressDirectory(download_path, archivePath);
       const compressEnd = Date.now();
       const processTime = (compressEnd - compressBegin) / 1000;
       span.addEvent('Compression completed', { 'process_time': processTime });
-      console.log('Compression completed in', processTime, 'seconds');
 
       const s3UploadBegin = Date.now();
-      console.log('Starting S3 upload...');
       const keyName = await storage_handler.upload(bucket, path.join(output_prefix, `${key}.zip`), archivePath);
       const s3UploadStop = Date.now();
       const uploadTime = (s3UploadStop - s3UploadBegin) / 1000;
       span.addEvent('S3 upload completed', { 'upload_time': uploadTime });
-      console.log('S3 upload completed in', uploadTime, 'seconds');
 
       span.setAttribute('upload_size', archiveSize);
 
       span.end();
-      console.log('Span ended.');
 
       return {
         result: {
@@ -140,7 +127,6 @@ exports.handler = async function(event) {
       span.recordException(err);
       span.setStatus({ code: 2, message: 'Handler error' });
       span.end();
-      console.error('Error:', err);
       throw err;
     }
   });
