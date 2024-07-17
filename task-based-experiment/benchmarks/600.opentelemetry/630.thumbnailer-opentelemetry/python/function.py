5acd582dd85ea8da9b17420ea06821c8c86da31a
A Nõu <43732190+andersnou@users.noreply.github.com>
2024-07-17 10:05:03 +0300

    Update function.py

--- task-based-experiment/benchmarks/600.opentelemetry/630.thumbnailer-opentelemetry/python/function.py
+++ task-based-experiment/benchmarks/600.opentelemetry/630.thumbnailer-opentelemetry/python/function.py
@@ -22,24 +22,14 @@ resource = Resource(attributes={
 trace.set_tracer_provider(TracerProvider(resource=resource))
 tracer = trace.get_tracer("function")
 
-# Setup the OTLP exporter to send traces to the collector using HTTP
 otlp_exporter = OTLPSpanExporter(
     endpoint="http://192.168.1.104:4317",
     insecure=True
 )
 
-# Setup the span processor with the exporter
 span_processor = SimpleSpanProcessor(otlp_exporter)
 trace.get_tracer_provider().add_span_processor(span_processor)
 
-
-# Disk-based solution
-#def resize_image(image_path, resized_path, w, h):
-#    with Image.open(image_path) as image:
-#        image.thumbnail((w,h))
-#        image.save(resized_path)
-
-# Memory-based solution
 def resize_image(image_bytes, w, h):
     with tracer.start_as_current_span("resize_image") as span:
         span.set_attribute("width", w)
