aff25e053b9d9289f2bea383f2fa6c5366e50596
Anders Nõu <anders.nou@gmail.com>
2024-09-02 10:38:17 +0200

    Experiment 3

--- e3-causes-of-overhead/e3.1-cprofile/request_based.py
+++ e3-causes-of-overhead/e3.1-cprofile/request_based.py
@@ -51,7 +51,7 @@ if __name__ == "__main__":
     profiling_data = manager.list()
 
     _endpoint = os.getenv("EXPERIMENT_ENDPOINT", "updates")
-    _iterations = int(os.getenv("EXPERIMENT_ITERATIONS", 100000))
+    _iterations = int(os.getenv("EXPERIMENT_ITERATIONS", 10))
     print("Running experiment {} with {} iterations".format(_endpoint, _iterations))
 
     # Run the experiment and collect profiling data
